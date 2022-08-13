103492b98d2fca20dcede6354464ea60624b590b
user <user@users-MacBook-Pro-2.local>
2022-08-13 14:32:22 -0700

    paymen and create-place-order-page

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,67 +1,66 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
 import {
-	AppBar,
-	Badge,
-	Box,
-	Button,
-	Container,
-	CssBaseline,
-	Link,
-  	Menu,
-  	MenuItem,
-	Switch,
-	ThemeProvider,
-	Toolbar,
-	Typography,
-} from "@mui/material";
-import Head from "next/head";
-import NextLink from "next/link";
-import classes from "../Utils/Classes";
+  AppBar,
+  Badge,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Link,
+  Menu,
+  MenuItem,
+  Switch,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import classes from '../utils/classes';
 import { useContext, useState } from 'react';
-import { Store } from "../utils/Store";
-import jsCookie from "js-cookie";
+import { Store } from '../utils/Store';
+import jsCookie from 'js-cookie';
 import { useRouter } from 'next/router';
-
 export default function Layout({ title, description, children }) {
-	const router = useRouter();
-	const { state, dispatch } = useContext(Store);
-	const { darkMode, cart, userInfo } = state;
-	const theme = createTheme({
-		components: {
-			MuiLink: {
-				defaultProps: {
-					underline: "hover",
-				},
-			},
-		},
-		typography: {
-			h1: {
-				fontSize: "1.6rem",
-				fontWeight: 400,
-				margin: "1rem 0",
-			},
-			h2: {
-				fontSize: "1.4rem",
-				fontWeight: 400,
-				margin: "1rem 0",
-			},
-		},
-		palette: {
-			mode: darkMode ? "dark" : "light",
-			primary: {
-				main: "#f0c000",
-			},
-			secondary: {
-				main: "#208080",
-			},
-		},
-	});
-	const darkModeChangeHandler = () => {
-		dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
-		const newDarkMode = !darkMode;
-		jsCookie.set("darkMode", newDarkMode ? "ON" : "OFF");
-	};
-	  const [anchorEl, setAnchorEl] = useState(null);
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+  const { darkMode, cart, userInfo } = state;
+  const theme = createTheme({
+    components: {
+      MuiLink: {
+        defaultProps: {
+          underline: 'hover',
+        },
+      },
+    },
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    jsCookie.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
+  const [anchorEl, setAnchorEl] = useState(null);
   const loginMenuCloseHandler = (e, redirect) => {
     setAnchorEl(null);
     if (redirect) {
@@ -76,48 +75,49 @@ export default function Layout({ title, description, children }) {
     dispatch({ type: 'USER_LOGOUT' });
     jsCookie.remove('userInfo');
     jsCookie.remove('cartItems');
-	jsCookie.remove('shippingAddress');
+    jsCookie.remove('shippingAddress');
+    jsCookie.remove('paymentMethod');
     router.push('/');
   };
-	return (
-		<>
-			<Head>
-				<title>{title ? `${title} - Saikat Mart` : "Saikat Mart"}</title>
-				{description && <meta name="description" content={description}></meta>}
-			</Head>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<AppBar position="static" sx={classes.appbar}>
-					<Toolbar sx={classes.toolbar}>
-						<Box display="flex" alignItems="center">
-							<NextLink href="/" passHref>
-								<Link>
-									<Typography sx={classes.brand}>Saikat Mart</Typography>
-								</Link>
-							</NextLink>
-						</Box>
-						<Box>
-							<Switch
-								checked={darkMode}
-								onChange={darkModeChangeHandler}
-							></Switch>
-							<NextLink href="/cart" passHref>
-								<Link>
-									<Typography component="span">
-										{cart.cartItems.length > 0 ? (
-											<Badge
-												color="secondary"
-												badgeContent={cart.cartItems.length}
-											>
-												Cart
-											</Badge>
-										) : (
-											"Cart"
-										)}
-									</Typography>
-								</Link>
-							</NextLink>
-							{userInfo ? (
+  return (
+    <>
+      <Head>
+        <title>{title ? `${title} - Sanity Amazona` : 'Sanity Amazona'}</title>
+        {description && <meta name="description" content={description}></meta>}
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" sx={classes.appbar}>
+          <Toolbar sx={classes.toolbar}>
+            <Box display="flex" alignItems="center">
+              <NextLink href="/" passHref>
+                <Link>
+                  <Typography sx={classes.brand}>amazona</Typography>
+                </Link>
+              </NextLink>
+            </Box>
+            <Box>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>
+                  <Typography component="span">
+                    {cart.cartItems.length > 0 ? (
+                      <Badge
+                        color="secondary"
+                        badgeContent={cart.cartItems.length}
+                      >
+                        Cart
+                      </Badge>
+                    ) : (
+                      'Cart'
+                    )}
+                  </Typography>
+                </Link>
+              </NextLink>
+              {userInfo ? (
                 <>
                   <Button
                     aria-controls="simple-menu"
@@ -142,21 +142,21 @@ export default function Layout({ title, description, children }) {
                     <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
                   </Menu>
                 </>
-							) : (
-								<NextLink href="/login" passHref>
-									<Link>Login</Link>
-								</NextLink>
-							)}
-						</Box>
-					</Toolbar>
-				</AppBar>
-				<Container component="main" sx={classes.main}>
-					{children}
-				</Container>
-				<Box component="footer" sx={classes.footer}>
-					<Typography>All rights reserved. Saikat Mart.</Typography>
-				</Box>
-			</ThemeProvider>
-		</>
-	);
+              ) : (
+                <NextLink href="/login" passHref>
+                  <Link>Login</Link>
+                </NextLink>
+              )}
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Container component="main" sx={classes.main}>
+          {children}
+        </Container>
+        <Box component="footer" sx={classes.footer}>
+          <Typography>All rights reserved. Sanity Amazona.</Typography>
+        </Box>
+      </ThemeProvider>
+    </>
+  );
 }
